5266a95b32471fdd0dea872336b73f2a5aa14c47
aviral5rivastava <aviral.lonewolf@gmail.com>
2022-12-15 22:37:16 +0530

    final changes set, project done

---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React from "react"
+import "../style.css"
+export default function Header() {
+    return (
+        <header className="header">
+            <h2 className="header--title">Meme Generator</h2>
+        </header>
+    )
+}
